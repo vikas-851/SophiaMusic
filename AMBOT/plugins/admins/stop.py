cbb85e02302ee96296498dd5e0619b60e4b85075
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-12-23 02:16:44 +0530

    Add files via upload

---
+++ AMBOT/plugins/admins/stop.py
@@ -0,0 +1,23 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from AMBOT import app
+from AMBOT.core.call import Anony
+from AMBOT.utils.database import set_loop
+from AMBOT.utils.decorators import AdminRightsCheck
+from AMBOT.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await Anony.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
